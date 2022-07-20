8405fd747ff2a1dc36b236c737c25ad082a2b260
zengxiangqi1031 <416428406@qq.com>
2022-07-20 14:48:14 +0800

    Update Test.java

--- src/main/java/com/zxq/Test.java
+++ src/main/java/com/zxq/Test.java
@@ -2,7 +2,7 @@ package com.zxq;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("22211111");
+        System.out.println("4444444444");
         System.out.println("2222222222");
     }
 }
