368837a785225cf196549ce1cb39641612ce5fef
zengxiangqi1031 <416428406@qq.com>
2022-07-20 15:08:00 +0800

    test

--- src/main/java/com/zxq/Test.java
+++ src/main/java/com/zxq/Test.java
@@ -2,7 +2,7 @@ package com.zxq;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("oooollll");
+        System.out.println("oooowwwww");
         System.out.println("2222222222");
     }
 }
